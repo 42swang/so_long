9c0dab4cf8484b71a39e394fc3b05dbaa85c77f5
swang <swang@MacBook-sh.local>
2021-08-31 17:12:20 +0900

    add error-message part

--- src/ft_error.c
+++ src/ft_error.c
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/05 11:14:35 by swang             #+#    #+#             */
-/*   Updated: 2021/08/31 15:55:41 by swang            ###   ########.fr       */
+/*   Updated: 2021/08/31 17:08:46 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,17 @@
 void	ft_error(int e)
 {
 	if (e == MAP_ERROR)
+	{
 		write(2, "Error\n", 6);
+		write(1, "The map must be composed of only 5 possible characters\n", 55);
+	}
+	else if (e == MAP_WALL_ERROR)
+	{
+		write(2, "Error\n", 6);
+		write(1,"The map must be closed/surrounded by walls\n", 43);
+	}
+	else
+	{
+		write(2, "Error\n", 6);
+	}
 }
