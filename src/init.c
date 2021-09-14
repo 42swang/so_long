6e38f2e6f194b0165d26f0d8bec5f5d107fb99e9
swang <swang@MacBook-sh.local>
2021-09-14 17:08:54 +0900

    add t_data initialize func

---
+++ src/init.c
@@ -0,0 +1,17 @@
+#include "../include/so_long.h"
+
+void	init_data(t_data *data)
+{
+	data->mlx = 0;
+	data->win = 0;
+	data->img = 0;
+	data->map.collect = 0;
+	data->map.exit = 0;
+	data->map.position = 0;
+	data->map.map_height = 0;
+	data->map.map_width = 0;
+
+
+
+
+}
