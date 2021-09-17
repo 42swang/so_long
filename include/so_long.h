66a01993ff3e8086395bfc9558f3915528e9b2a6
swang <swang@MacBook-sh.local>
2021-09-17 12:19:28 +0900

    add struct for img

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 17:17:57 by swang             #+#    #+#             */
-/*   Updated: 2021/09/14 16:36:23 by swang            ###   ########.fr       */
+/*   Updated: 2021/09/17 11:26:12 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,23 @@
 # include "../gnl/get_next_line.h"
 # include "../mlx/mlx.h"
 
-# define MAP_WALL_ERROR 103
+# define NO_PRAM 101
+# define FD_ERROR 102
+# define IMG_ERROR 103
 # define MAP_ERROR 104
-# define INFO_COUNT 105
+# define MLX_FAIL 105
+# define NOT_SQUARE 106
+
+typedef struct s_imgs
+{
+	void	*item;
+	void	*player;
+	void	*door_closed;
+	void	*door;
+	void	*wall;
+	void	*floor;
+	void	*spikes;
+}			t_imgs;
 
 typedef struct s_map
 {
@@ -32,25 +46,49 @@ typedef struct s_map
 	int	map_height;
 }	t_map;
 
+typedef	struct s_spike
+{
+	int	a;
+	int b;
+	int c;
+	int d;
+}	t_spike;
+
 typedef struct s_data
 {
 	void	*mlx;
 	void	*win;
 	void	*img;
+	int		fd;
+	int		count;
+	char	*name;
+	t_imgs	image;
 	t_map	map;
+	t_spike	spk;
 }	t_data;
 
 /* open map */
 int	open_map(t_data *data, char *str);
 
-/* check map */
-int	check_long_wall(char *str);
-int	check_short_wall(char *str);
-int	check_obj(char *str, t_data *data);
+/* ft_check map */
+int	ft_check_long_wall(char *str);
+int	ft_check_short_wall(char *str);
+int	ft_check_obj(char *str, t_data *data);
+
+/* init */
+void	init_data(t_data *data);
+void	init_image(t_data *data);
 
 /* src */
-void	init_data(t_data *data);
-void	ft_error(int e);
+void	window(t_data *data);
+void	put_image(t_data *data);
+void	put_floor(t_data *data);
+void	put_element(t_data *data);
+void	put_element2(int line, char *str, t_data *data);
+void	put_spikes(t_data *data, int x, int y);
+/* utils*/
 int	ft_strcmp(char *s1, char *s2);
+void	ft_error(int i);
+
 
 #endif
