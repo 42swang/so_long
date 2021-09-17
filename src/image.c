51ca88397aa1d88a6670f440750bdcfe023725ca
swang <swang@MacBook-sh.local>
2021-09-17 20:19:47 +0900

    fix spike disappear when mlx_loop_hook

--- src/image.c
+++ src/image.c
@@ -1,9 +1,10 @@
 #include "../include/so_long.h"
 
-void	put_image(t_data *data)
+int		put_image(t_data *data)
 {
 	put_floor(data);
 	put_element(data);
+	return (1);
 }
 
 void	put_floor(t_data *data)
@@ -24,7 +25,7 @@ void	put_floor(t_data *data)
 	}
 }
 
-void	put_spikes(t_data *data, int x, int y)
+static void	put_spikes(t_data *data, int x, int y)
 {
 	int	w_half;
 	int h_half;
@@ -34,21 +35,25 @@ void	put_spikes(t_data *data, int x, int y)
 	if (w_half > x && h_half > y && data->spk.a == 1)
 	{
 		mlx_put_image_to_window(data->mlx, data->win, data->image.spikes, x * 32, y * 32);
+		data->map.map[y][x] = 'S';
 		data->spk.a = 0;
 	}
 	else if (w_half < x && h_half > y && data->spk.b == 1)
 	{
 		mlx_put_image_to_window(data->mlx, data->win, data->image.spikes, x * 32, y * 32);
+		data->map.map[y][x] = 'S';
 		data->spk.b = 0;
 	}
 	else if (w_half > x && h_half < y && data->spk.c == 1)
 	{
 		mlx_put_image_to_window(data->mlx, data->win, data->image.spikes, x * 32, y * 32);
+		data->map.map[y][x] = 'S';
 		data->spk.c = 0;
 	}
 	else if (w_half < x && h_half < y && data->spk.d == 1)
 	{
 		mlx_put_image_to_window(data->mlx, data->win, data->image.spikes, x * 32, y * 32);
+		data->map.map[y][x] = 'S';
 		data->spk.d = 0;
 	}
 }
@@ -72,12 +77,19 @@ void	put_element(t_data *data)
 				mlx_put_image_to_window(data->mlx, data->win, data->image.item, x * 32, y * 32);
 			else if (arr[y][x] == 'P')
 				mlx_put_image_to_window(data->mlx, data->win, data->image.player, x * 32, y * 32);
-			else if (arr[y][x] == 'E')
-				mlx_put_image_to_window(data->mlx, data->win, data->image.door_closed, x * 32, y * 32);
-			else if (arr[y][x] == '0')
+			else if (arr[y][x] == 'p')
+				mlx_put_image_to_window(data->mlx, data->win, data->image.left_player, x * 32, y * 32);
+			else if (arr[y][x] == 'E' && data->map.collect != 0)
+				mlx_put_image_to_window(data->mlx, data->win, data->image.door, x * 32, y * 32);
+			else if (arr[y][x] == 'E' && data->map.collect == 0)
+				mlx_put_image_to_window(data->mlx, data->win, data->image.stair, x * 32, y * 32);
+			else if (arr[y][x] == 'S')
+				mlx_put_image_to_window(data->mlx, data->win, data->image.spikes, x * 32, y * 32);
+			else if (arr[y][x] == '0' && data->spk.s == 1)
 				put_spikes(data, x, y);
 			x++;
 		}
 		y++;
 	}
+	data->spk.s = 0;
 }
