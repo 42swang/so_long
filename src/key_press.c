2655447d3eac68103c16f1b03cef687fbd3e0d2e
swang <swang@MacBook-sh.local>
2021-09-19 21:23:33 +0900

    fix movement out

--- src/key_press.c
+++ src/key_press.c
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 01:32:26 by swang             #+#    #+#             */
-/*   Updated: 2021/09/19 21:02:49 by swang            ###   ########.fr       */
+/*   Updated: 2021/09/19 21:17:12 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,19 @@ void	press_w(t_data *data)
 	char	c;
 
 	c = data->map.map[data->py - 1][data->px];
-	data->move++;
 	if (c == 'C')
 		data->map.collect--;
 	else if (c == 'E')
 	{
 		if (data->map.collect == 0)
-			ft_goal(data->move);
+			ft_goal(++data->move);
 		return ;
 	}
 	data->map.map[data->py - 1][data->px] = 'P';
 	data->map.map[data->py][data->px] = 'z';
 	if (data->py > 1)
 		data->py--;
-	ft_shell_out(data->move);
+	ft_shell_out(++data->move);
 }
 
 void	press_a(t_data *data)
@@ -38,20 +37,19 @@ void	press_a(t_data *data)
 	char	c;
 
 	c = data->map.map[data->py][data->px - 1];
-	data->move++;
 	if (c == 'C')
 		data->map.collect--;
 	else if (c == 'E')
 	{
 		if (data->map.collect == 0)
-			ft_goal(data->move);
+			ft_goal(++data->move);
 		return ;
 	}
 	data->map.map[data->py][data->px - 1] = 'p';
 	data->map.map[data->py][data->px] = 'z';
 	if (data->px > 1)
 		data->px--;
-	ft_shell_out(data->move);
+	ft_shell_out(++data->move);
 }
 
 void	press_s(t_data *data)
@@ -59,20 +57,19 @@ void	press_s(t_data *data)
 	char	c;
 
 	c = data->map.map[data->py + 1][data->px];
-	data->move++;
 	if (c == 'C')
 		data->map.collect--;
 	else if (c == 'E')
 	{
 		if (data->map.collect == 0)
-			ft_goal(data->move);
+			ft_goal(++data->move);
 		return ;
 	}	
 	data->map.map[data->py + 1][data->px] = 'P';
 	data->map.map[data->py][data->px] = 'z';
 	if (data->py < data->map.map_height - 2)
 		data->py++;
-	ft_shell_out(data->move);
+	ft_shell_out(++data->move);
 }
 
 void	press_d(t_data *data)
@@ -80,18 +77,17 @@ void	press_d(t_data *data)
 	char	c;
 
 	c = data->map.map[data->py][data->px + 1];
-	data->move++;
 	if (c == 'C')
 		data->map.collect--;
 	else if (c == 'E')
 	{
 		if (data->map.collect == 0)
-			ft_goal(data->move);
+			ft_goal(++data->move);
 		return ;
 	}
 	data->map.map[data->py][data->px + 1] = 'P';
 	data->map.map[data->py][data->px] = 'z';
 	if (data->px < data->map.map_width - 2)
 		data->px++;
-	ft_shell_out(data->move);
+	ft_shell_out(++data->move);
 }
