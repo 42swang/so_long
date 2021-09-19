9e54f1223606c8fcacb6b4dafb09ad687bbc6b54
swang <swang@MacBook-sh.local>
2021-09-19 21:22:05 +0900

    remove bonus part

--- include/so_long.h
+++ include/so_long.h
@@ -6,14 +6,13 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 17:17:57 by swang             #+#    #+#             */
-/*   Updated: 2021/09/18 02:08:35 by swang            ###   ########.fr       */
+/*   Updated: 2021/09/19 20:14:47 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
@@ -38,7 +37,6 @@ typedef struct s_imgs
 	void	*stair;
 	void	*wall;
 	void	*floor;
-	void	*spikes;
 }			t_imgs;
 
 typedef struct s_map
@@ -51,15 +49,6 @@ typedef struct s_map
 	int		map_height;
 }	t_map;
 
-typedef struct s_spike
-{
-	int	a;
-	int	b;
-	int	c;
-	int	d;
-	int	s;
-}	t_spike;
-
 typedef struct s_data
 {
 	void	*mlx;
@@ -68,9 +57,9 @@ typedef struct s_data
 	int		fd;
 	int		px;
 	int		py;
+	int		move;
 	t_imgs	image;
 	t_map	map;
-	t_spike	spk;
 }	t_data;
 
 /* map */
@@ -89,7 +78,6 @@ int		check_element(t_data *data);
 void	init_data(t_data *data);
 void	init_image(t_data *data);
 void	init_map(t_data *data);
-void	init_spk(t_data *data);
 void	get_image(t_data *data);
 
 /* window*/
@@ -111,6 +99,7 @@ void	press_d(t_data *data);
 void	press_x(t_data *data);
 
 /* utils*/
+char	*ft_itoa(int n);
 char	**ft_split(char const *s, char c);
 char	*ft_substr(const char *s, unsigned int start, int len);
 void	*ft_calloc(size_t count, size_t size);
@@ -119,10 +108,10 @@ size_t	ft_strlen(const char *s);
 int		ft_strcmp(char *s1, char *s2);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s1);
+void	ft_shell_out(int move);
 
 /* ft_end*/
 void	ft_error(int i);
-void	ft_goal(int i);
-void	ft_die(int i);
+void	ft_goal(int move);
 
 #endif
